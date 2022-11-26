071c668e14a04bc53983af081f2e3d750bf9f6a1
Ryan Menow <89487116+menowryan@users.noreply.github.com>
2022-11-26 16:02:24 -0500

    Main Menu Initial

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,6 +13,31 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Future code for Level Switching
+        /*
+        if (playerScore == 3)
+        {
+            if(scene == "PingLevel2" || scene == "PingLevel3")
+            {
+                SceneManager.LoadScene(scene);
+                aiScore = 0;
+                playerScore = 0;
+            }
+            else
+            {
+                timer.GetComponent<PingTimer>().beatGame = true;
+                timer.GetComponent<PingTimer>().WinDisplay();
+                aiScore = 0;
+                playerScore = 0;
+                Time.timeScale = 0.0f;
+            }
+        }
+        if (aiScore == 3)
+        {
+            SceneManager.LoadScene("MainMenu");
+            aiScore = 0;
+            playerScore = 0;
+        }
+        */
     }
 }
