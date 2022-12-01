99d39b93fe38376563c7e92e210784d668c10fc7
Sadie Fischer <38534861+acpicklepearl@users.noreply.github.com>
2022-11-30 22:33:29 -0800

    elfsss

--- Assets/Materials/Elf/FBX/Elf/PlayerFollow.cs
+++ Assets/Materials/Elf/FBX/Elf/PlayerFollow.cs
@@ -4,6 +4,9 @@ public class PlayerFollow : MonoBehaviour
 {
     public Transform Playerpos;
     UnityEngine.AI.NavMeshAgent agent;
+     public float PlayerRange = 40.0f;
+    int count = 0;
+    float walkRadius = 6.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,15 +14,23 @@ public class PlayerFollow : MonoBehaviour
     }
     void Update()
     {
-        
-          agent.destination = Playerpos.position;    
-        
+        float distance = Vector3.Distance(transform.position, Playerpos.transform.position);
+        if(distance < PlayerRange) {
+		    agent.destination = Playerpos.position; 
+        } else {
+            Vector3 randomDirection = Random.insideUnitSphere * walkRadius;
+            randomDirection += transform.position;
+            agent.destination = randomDirection;
+        }      
     }
 
     void OnTriggerEnter (Collider collider) {
 		GameObject collidedWith = collider.gameObject;
         if (collidedWith.tag == gameObject.tag) {
-			Destroy (gameObject);
+			count++;
+        }
+        if(count > 20) {
+            Destroy (gameObject);
 			Destroy (collidedWith);
         }
     }
--- Assets/Materials/Elf/FBX/Elf/PlayerRUN.cs
+++ Assets/Materials/Elf/FBX/Elf/PlayerRUN.cs
@@ -5,6 +5,8 @@ public class PlayerRUN : MonoBehaviour
     public Transform Playerpos;
     UnityEngine.AI.NavMeshAgent agent;
     public float PlayerRange = 4.0f;
+    int count = 0;
+    float walkRadius = 6.0f;
     void Start()
     {
        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
@@ -13,16 +15,23 @@ public class PlayerRUN : MonoBehaviour
     {
 	float distance = Vector3.Distance(transform.position, Playerpos.transform.position);
         if(distance < PlayerRange) {
-		Vector3 dirToPlayer = transform.position - Playerpos.transform.position;
-	Vector3 newPos = transform.position + dirToPlayer;
-agent.SetDestination(newPos);
-}
+		    Vector3 dirToPlayer = transform.position - Playerpos.transform.position;
+	        Vector3 newPos = transform.position + dirToPlayer;
+            agent.SetDestination(newPos);
+        } else {
+            Vector3 randomDirection = Random.insideUnitSphere * walkRadius;
+            randomDirection += transform.position;
+            agent.destination = randomDirection;
+        }
     }
 
     void OnTriggerEnter (Collider collider) {
 		GameObject collidedWith = collider.gameObject;
         if (collidedWith.tag == gameObject.tag) {
-			Destroy (gameObject);
+			count++;
+        }
+        if(count > 20) {
+            Destroy (gameObject);
 			Destroy (collidedWith);
         }
     }
