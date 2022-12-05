e077da808c452d64e6fcf3135143665c1150454b
Sadie Fischer <38534861+acpicklepearl@users.noreply.github.com>
2022-12-05 18:30:15 -0500

    final

--- Assets/Scripts/MovementFianl.cs
+++ Assets/Scripts/MovementFianl.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MovementFianl : MonoBehaviour
 {
-    float speed = 2f;
+    float speed = 0.5f;
     private int count = 0;
     Vector3 pos;
     public GameObject zop;
@@ -18,11 +18,11 @@ public class MovementFianl : MonoBehaviour
         if(transform.position.y < 1.5f) {
             float newY = Time.time * speed + pos.y;
             transform.position = new Vector3(transform.position.x, newY, transform.position.z);
-        } else if(count <= 900) {
-            count++;
-            zop.transform.Rotate(0, -0.2f, 0);
+        } else if(count <= 3600) {
+            count++; 
+            zop.transform.Rotate(0, -0.05f, 0);
             pos = transform.position;
-        } else if(count >= 900 && transform.position.z < -6.5f) {
+        } else if(count >= 3600 && transform.position.z < -6.5f) {
 
             float newZ = Time.time * 0.1f + pos.z;
             transform.position = new Vector3(transform.position.x, transform.position.y, newZ);
