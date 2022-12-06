04301d6ec64e66da59039878a28840e2538f4d75
Ryan Menow <89487116+menowryan@users.noreply.github.com>
2022-12-05 21:53:53 -0500

    Fixed Final Scene Issue
    Moved camera up to actual see the scene play out

--- Assets/Scripts/ZopMove.cs
+++ Assets/Scripts/ZopMove.cs
@@ -4,22 +4,19 @@ using UnityEngine;
 
 public class ZopMove : MonoBehaviour
 {
-
     private float spin = 30f;
     private float spin2 = 90;
     private float elapsedTime = 0f;
 
-    void Update() {
-
+    void Update()
+    {
         elapsedTime += Time.deltaTime;
         if (elapsedTime > spin)
-        {   
-            while(spin2 > elapsedTime - 30) {
+        {
+            while (spin2 > elapsedTime - 30)
+            {
                 transform.Rotate(0, 2, 0);
-
             }
         }
-        
-        
     } 
 }
